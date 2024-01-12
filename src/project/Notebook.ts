0e1b6ce6f34e5cb47daa029715595c136da5628f
Swa <swa@orbifold.net>
2024-01-12 15:31:34 +0100

    Notebook changes.

--- src/project/Notebook.ts
+++ src/project/Notebook.ts
@@ -22,7 +22,7 @@ export class Notebook {
    * @param beforeOrAfter {string} Either "before" or "after". Default is "after".
    */
   public addCell(
-    cell: NotebookCell|null=null,
+    cell: NotebookCell | null = null,
     cellId: string | null = null,
     beforeOrAfter: string = "after"
   ) {
@@ -33,19 +33,51 @@ export class Notebook {
       // add at the end
       this.cells.push(cell);
     } else {
-      const index = this.cells.findIndex((c) => c.id === cellId);
-      if (index === -1) {
+      // the id with the direction forms in a way a primary key
+      const inputIndex = this.cells.findIndex(
+        (c) => c.id === cellId && c.direction === "input"
+      );
+      // note that we check the reference id here and not the cell id
+      // this is because the id of the cell and the message id are identical
+      const outputIndex = this.cells.findIndex(
+        (c) => c.inputCellId === cellId && c.direction === "output"
+      );
+
+      if (inputIndex === -1 && outputIndex === -1) {
         throw new Error(`Cannot find a cell with id ${cellId}.`);
       }
-      if (beforeOrAfter === "after") {
-        this.cells.splice(index + 1, 0, cell);
+      if (cell.direction === "input") {
+        // after the output if available, but before always means before the input
+        if (beforeOrAfter === "after") {
+          this.cells.splice(
+            (outputIndex > 0 ? outputIndex : inputIndex) + 1,
+            0,
+            cell
+          );
+        } else {
+          this.cells.splice(inputIndex, 0, cell);
+        }
       } else {
-        this.cells.splice(index, 0, cell);
+        // check wether the output is new or replacing an existing one
+        if (beforeOrAfter === "before") {
+          throw new Error("Cannot add an output cell before an input cell.");
+        }
+        if (outputIndex > -1) {
+          this.cells.splice(outputIndex, 1, cell);
+        } else {
+          this.cells.splice(inputIndex + 1, 0, cell);
+        }
       }
     }
-	return cell;
+    return cell;
   }
   public get idSequence() {
-	return this.cells.map((c) => c.id);
+    return this.cells.map((c) => c.id);
+  }
+  public getCellById(id: string) {
+    return this.cells.find((c) => c.id === id) || null;
+  }
+  public cellIdExists(id: string) {
+    return this.cells.some((c) => c.id === id);
   }
 }
--- src/project/NotebookCell.ts
+++ src/project/NotebookCell.ts
@@ -3,7 +3,8 @@ import { Message } from "..";
 
 export class NotebookCell {
 	public typeName = "NotebookCell";
-
+	// if the direction is output this contains the reference to the input cell
+	public inputCellId: string|null = null;
 	constructor(public notebook: any, public message: Message, public direction = "input", public index: string = "", public id: string = Utils.id()) {
 	}
 }
